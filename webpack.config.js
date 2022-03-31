af99fbfd930a62fec2ea494b4a2451daeab8ff1f
Roman Rakhmanin <r.rahmanin@ficto.ru>
2022-03-31 10:57:47 +0300

    remove comments

--- webpack.config.js
+++ webpack.config.js
@@ -6,18 +6,13 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 /*-------------------------------------------------*/
 const srcPath = path.resolve(__dirname, "./src/");
 module.exports = {
-
-  // режим webpack оптимизации
   mode: ( 'development' === process.env.NODE_ENV ? 'development' : 'production' ),
   entry: ('development' === process.env.NODE_ENV ? path.resolve(srcPath, "index.dev.js") : path.resolve(srcPath, "index.prod.js")),
-  // выходные файлы и чанки
   output: {
     path: path.resolve( __dirname, 'dist' ),
     clean: true,
     filename: 'build/[name].js',
   },
-
-  // module/loaders configuration
   module: {
     rules: [
       {
@@ -31,53 +26,37 @@ module.exports = {
       }
     ]
   },
-
-  // webpack плагины
   plugins: [
-
-    // выделение css во внешний файл таблицы стилей
     new MiniCssExtractPlugin( {
       filename: 'build/styles.css'
     } ),
-
-    // подготовка HTML файла с ресурсами
     new HtmlWebpackPlugin({
       title: 'My App',
       template: path.join(__dirname, 'src', 'index.html')
     }),
   ],
 
-  // настройка распознавания файлов
   resolve: {
-
-    // расширения файлов
     extensions: [ '.js', '.jsx', '.scss' ],
   },
-
-  // webpack оптимизации
   optimization: {
     splitChunks: {
       cacheGroups: {
         default: false,
         vendors: false,
-
         vendor: {
-          chunks: 'all', // both : consider sync + async chunks for evaluation
-          name: 'vendor', // имя чанк-файла
-          test: /node_modules/, // test regular expression
+          chunks: 'all',
+          name: 'vendor',
+          test: /node_modules/,
         }
       }
     }
   },
-
-  // настройки сервера разработки
   devServer: {
-    port: 3001,
+    port: 3000,
     historyApiFallback: true,
     open:true
   },
-
-  // генерировать source map
   devtool: 'source-map'
 
 };
